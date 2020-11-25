5a8bb1452cc8d5a630b9fb7a49b1f35d6efe8b4c
Rashawn Raiford <raiford87@gmail.com>
2020-11-24 21:57:46 -0500

    Added method to EmployeeDB to delete employee

--- employeeDB.js
+++ employeeDB.js
@@ -91,7 +91,32 @@ class EmployeeDB{
         }) 
     }
 
+    deleteEmployee(id){
 
+        return new Promise((resolve, reject) => {
+            this.#connection.query("SELECT * FROM employee Where manager_id = ? LIMIT 1", id, (err, employees) => {
+                if(err) throw err;
+                
+                if(employees.length > 0){
+                    this.#connection.query("UPDATE employee SET manager_id = null WHERE manager_id = ?", id, (err, res) => {
+                        if(err) throw err;
+                        
+                        this.#connection.query("DELETE FROM employee WHERE id = ?", id, (err, res) => {
+                            if(err) throw err;
+                            resolve(res);
+                            
+                        })
+                    })
+                }else{
+                    this.#connection.query("DELETE FROM employee WHERE id = ?", id, (err, res) => {
+                        if(err) throw err;
+                        resolve(res);
+                        
+                    })
+                }                
+            })
+        })
+    }
 
     getAllRoles(){
         return new Promise((resolve, reject) => {
