9ee323fa92b54afff0df6811d64af0dde2d7df9e
Rashawn Raiford <raiford87@gmail.com>
2020-11-24 23:26:41 -0500

    Added method to EmployeeDB to delete a department

--- employeeDB.js
+++ employeeDB.js
@@ -164,6 +164,15 @@ class EmployeeDB{
         })       
     }
 
+    deleteDepartment(id){
+        return new Promise((resolve, reject) => {
+            this.#connection.query("DELETE FROM department WHERE id = ?", id, (err, res) => {
+                if(err) throw err;
+                resolve(res);              
+            })
+        })    
+    }
+
     close(){
         this.#connection.end();
     }
